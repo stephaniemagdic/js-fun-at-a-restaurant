ee7effd8324ab35ede2dff856a6577d6d969b6a8
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-05-14 08:03:17 -0600

    Add meals.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return menuItemName = "Delicious " + name;
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  };
+
+  return menuItem;
+}
+
+function addIngredients(ingredient, ingredientsList) {
+  if (!ingredientsList.includes(ingredient)) {
+    return ingredientsList.push(ingredient);
+  }
+}
+
+function formatPrice(price) {
+  return "$" + price;
+}
+
+function decreasePrice(price){
+  var tenPercentDiscount = price * .10;
+  return price - tenPercentDiscount;
+}
+
+function createRecipe(title, ingredients, menuItemType){
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-

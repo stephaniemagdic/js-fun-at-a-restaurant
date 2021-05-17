5fe82d44325188130f5b778ef2ee47dcc32256b2
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-05-16 18:14:15 -0600

    AddChef.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,28 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(customerName, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${customerName}!`;
+    } else {
+        return `Hello, ${customerName}!`;
+    }
+  }
+
+  checkForFood(itemToCheck) {
+    var itemMenu = itemToCheck.type;
+    var restaurant = this.restaurant;
+    for (var i = 0; i < restaurant.menus[itemMenu].length; i++) {
+      if (itemToCheck.name === restaurant.menus[itemMenu][i].name) {
+        return `Yes, we're serving ${itemToCheck.name} today!`;
+      }
+    }
+    return `Sorry, we aren't serving ${itemToCheck.name} today.`
   }
 }
+
 
 module.exports = Chef;
